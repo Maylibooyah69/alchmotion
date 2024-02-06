0d9dc6755d25e6dbef1e2de96b359530d139ca3b
Hongyi <maylibooyah69@gmail.com>
2024-02-06 11:04:02 +0800

    hardcode window sizze

--- website/app/experimental/edit/page.js
+++ website/app/experimental/edit/page.js
@@ -2,7 +2,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Stage, Layer, Star, Text } from 'react-konva';
-
+// https://konvajs.org/react-konva/
 function generateShapes() {
     const width = 400;
     const height = 400;
